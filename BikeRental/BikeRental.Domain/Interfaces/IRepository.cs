faadb4b1691a6e4323a03ca55980491d1b150014
dariatsyganova <tsda04@mail.ru>
2025-12-14 23:46:36 +0400

    created IRepository, changed .csproj

---
+++ BikeRental/BikeRental.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,34 @@
+namespace BikeRental.Domain.Interfaces;
+/// <summary>
+/// Generic repository interface that defines basic CRUD operations.
+/// </summary>
+public interface IRepository<TEntity>
+    where TEntity : class
+{
+    /// <summary>
+    /// Returns all entities.
+    /// </summary>
+    public Task<List<TEntity>> GetAll();
+
+    /// <summary>
+    /// Returns entity by id.
+    /// </summary>
+    public Task<TEntity?> GetById(int id);
+
+    /// <summary>
+    /// Adds a new entity and returns its generated id.
+    /// </summary>
+    public Task<int> Add(TEntity entity);
+
+    /// <summary>
+    /// Updates existing entity.
+    /// </summary>
+    public Task Update(TEntity entity);
+
+    /// <summary>
+    /// Deletes existing entity.
+    /// </summary>
+    public Task Delete(TEntity entity);
+}
+
+    
